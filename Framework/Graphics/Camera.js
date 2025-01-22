c3232862c4dd6dd7733171de16802da642c5f473
Sephelim <1001891@warwicksd.org>
2025-01-22 12:17:22 -0500

    fully "functional"camera added

--- Framework/Graphics/Camera.js
+++ Framework/Graphics/Camera.js
@@ -16,27 +16,76 @@
 
 // #region Module Dependencies
 
+import {Selenium_Graphics_Shaders} from "./Shaders.js";
+
 import {GLMatrix} from "../../Dependencies/GLMatrix.js";
+
+/**
+ * @import {Position} from "../Graphics.js"
+ */
 
 // #endregion Module Dependencies
 // #region Namespace Declaration
 
+/**
+ * The Selenium camera subnamespace of the camera space. This provides
+ * functionality for setting and moving the camera view matrix.
+ * @since 0.0.5
+ */
 var Selenium_Graphics_Camera = Selenium_Graphics_Camera || {};
 Selenium_Graphics_Camera.__proto__ = null;
 
+/**
+ * The actual position of the camera. Because of the isometric projection,
+ * the X axis goes to the bottom right (positive), the Z axis goes to the
+ * bottom (negative), and the Y axis goes to the bottom left (positive).
+ * @type {Position}
+ * @since 0.0.5
+ */
 Selenium_Graphics_Camera.Position = {
     x: 0,
     y: 0,
     z: 0
 };
 
-Selenium_Graphics_Camera.Matrix = GLMatrix.Mat4.create();
-
-GLMatrix.Mat4.translate(Selenium_Graphics_Camera.Matrix,
-    Selenium_Graphics_Camera.Matrix,
-    GLMatrix.Vec3.fromValues(Selenium_Graphics_Camera.Position.x,
+/**
+ * Set the view matrix of the given shader. This is an expensive function,
+ * and should only be done when absolutely necessary.
+ * @authors Sephelim
+ * @since 0.0.5
+ *
+ * @param {string} shader The shader to set. This must have a 4x4 matrix
+ *     uniform named view_matrix, or this call will do nothing.
+ */
+Selenium_Graphics_Camera.SetView = function(shader) {
+    const position_vector = GLMatrix.Vec3.fromValues(
+        Selenium_Graphics_Camera.Position.x,
         Selenium_Graphics_Camera.Position.y,
-        Selenium_Graphics_Camera.Position.z));
+        Selenium_Graphics_Camera.Position.z,
+    );
+
+    let position_matrix = GLMatrix.Mat4.create();
+    GLMatrix.Mat4.translate(
+        position_matrix, position_matrix, position_vector);
+
+    Selenium_Graphics_Shaders.SetUniform(
+        shader, "m4_view_matrix", position_matrix);
+};
+
+Selenium_Graphics_Camera.MoveUp = function(
+    amount) { Selenium_Graphics_Camera.Position.z -= amount; };
+
+Selenium_Graphics_Camera.MoveDown = function(
+    amount) { Selenium_Graphics_Camera.Position.z += amount; };
+
+Selenium_Graphics_Camera.MoveLeft = function(amount) {
+    Selenium_Graphics_Camera.Position.y -= amount;
+    Selenium_Graphics_Camera.Position.z -= amount / 2;
+};
+Selenium_Graphics_Camera.MoveRight = function(amount) {
+    Selenium_Graphics_Camera.Position.x -= amount;
+    Selenium_Graphics_Camera.Position.z -= amount / 2;
+};
 
 // #endregion Namespace Declaration
 // #region Module Exports
