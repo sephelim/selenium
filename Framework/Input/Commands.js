3e011772c5fa788ee1032e70fe777d9d537c88d3
Ian <boltz.ian@gmail.com>
2025-01-27 10:38:34 -0500

    started implemetation of command system

---
+++ Framework/Input/Commands.js
@@ -0,0 +1,25 @@
+/**
+ * @file Graphics/Camera.js
+ * @authors Ian
+ * @brief This file allows selenium programs to make commands, and process 
+ * commands. Commands could be inputed throuh the devtools console or an 
+ * ingame system.
+ *    
+ * @since 0.0.5
+ *
+ * @license AGPLv3
+ * @copyright (c) 2025 the Selenium Development Team
+ * This source file is under the AGPLv3. For information on what that
+ * means, see the LICENSE.md file that should have been provided alongside
+ * this project, or https://www.gnu.org/licenses/agpl-3.0.md.
+ */
+"use strict";
+
+// #region Namespace Declaration
+
+/**
+ * The Selenium commands subnamespace 
+ * @since 0.0.5
+ */
+var Selenium_Input_Commands = Selenium_Input_Commands || {};
+Selenium_Input_Commands.__proto__ = null;
